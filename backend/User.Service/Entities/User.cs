a18bfef3d9e78d1cd66cde1237b004e4256cc0ef
Soulaimane Ouhmida <105684836+Soulaimane07@users.noreply.github.com>
2024-11-08 14:17:10 +0100

    User service

---
+++ backend/User.Service/Entities/User.cs
@@ -0,0 +1,10 @@
+namespace User.Service.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; } // Consider hashing passwords
+    }
+}
--- backend/User.Service/Program.cs
+++ backend/User.Service/Program.cs
@@ -1,7 +1,17 @@
+using User.Service.Repositories;
+using Microsoft.EntityFrameworkCore;
+using User.Service.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+string postgresConnection = builder.Configuration.GetConnectionString("PostgresConnection");
+builder.Services.AddScoped<UsersRepository>(_ => new UsersRepository(postgresConnection));
 
 var app = builder.Build();
 
@@ -12,5 +22,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
